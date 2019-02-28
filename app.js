afc73541540b0d1a7336b36f911b43450fae8c61
HerrWolf <webmasterp2m@gmail.com>
2019-02-27 20:57:05 -0800

    Creada la funcion de listar tareas

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0];
 
 switch (comando) {
 
     case 'crear':
-        console.log('Crear tarea por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
 
     case 'listar':
-        console.log('Listar tareas por hacer');
+
+        let listado = porHacer.getListado();
+
+        for (let tarea of listado) {
+
+            console.log('============ Por Hacer ============'.green);
+            console.log(tarea.descripcion);
+            console.log(tarea.completado);
+            console.log('==================================='.green);
+        }
+
         break;
 
     case 'actualizar':
